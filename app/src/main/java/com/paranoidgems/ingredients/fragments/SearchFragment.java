957b26b91ca497bd82da3e14a398dd8bd582ad3b
Sudhanshu Mishra <mrsud94@gmail.com>
2014-08-31 04:52:41 +0530

    Dynamic view adding. Fixes #8.

--- app/src/main/java/com/paranoidgems/ingredients/fragments/SearchFragment.java
+++ app/src/main/java/com/paranoidgems/ingredients/fragments/SearchFragment.java
@@ -1,16 +1,23 @@
 package com.paranoidgems.ingredients.fragments;
 
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.paranoidgems.ingredients.R;
 
 public class SearchFragment extends Fragment{
+
+    Typeface tf;
+
     /**
      * This method is automatically called when activity creation completes.
      */
@@ -21,6 +28,10 @@ public class SearchFragment extends Fragment{
         Typeface tf = Typeface.createFromAsset(getActivity()
                         .getApplicationContext().getAssets(),
                 "fonts/roboto-regular.ttf");
+        setCard("Paneer Pakoda", "Missing Paneer, Olive oil and Flour.");
+        setCard("Cheese Pakoda", "Missing Cheese, Mustard oil and Flour.");
+        setCard("Franky", "Missing Onion, Olive oil and Green vegetables.");
+        setCard("Aloo Chokha", "Missing Potato and Mustard oil.");
     }
 
     @Override
@@ -30,5 +41,30 @@ public class SearchFragment extends Fragment{
         View rootView = inflater.inflate(R.layout.fragment_search, container,
                 false);
         return rootView;
+    }
+
+    private void setCard(String dishNameValue, String missingTextValue){
+        LayoutInflater vi = (LayoutInflater) getActivity()
+                .getApplicationContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = vi.inflate(R.layout.layout_dish, null);
+        TextView dishName = (TextView) v.findViewById(R.id.txtDishName);
+        dishName.setTypeface(tf);
+        dishName.setText(dishNameValue);
+        TextView missingText = (TextView) v.findViewById(R.id.txtMissingIngredients);
+        missingText.setTypeface(tf);
+        missingText.setText(missingTextValue);
+        ImageView dishImage = (ImageView) v.findViewById(R.id.imgDish);
+        dishImage.setImageDrawable(getActivity().getResources()
+                .getDrawable(R.drawable.halloween));
+
+        View container = getActivity().findViewById(R.id.sub_container_fragment_search);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, 0, 0, 20);
+
+        ((ViewGroup) container).addView(v, layoutParams);
     }
 }
--- app/src/main/java/com/paranoidgems/ingredients/views/DishView.java
+++ app/src/main/java/com/paranoidgems/ingredients/views/DishView.java
@@ -7,7 +7,13 @@ package com.paranoidgems.ingredients.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.paranoidgems.ingredients.R;
+import com.paranoidgems.ingredients.content.Dish;
 
 /**
  * This class is a custom view to show a dish as a search result.
@@ -15,17 +21,28 @@ import android.widget.RelativeLayout;
  */
 public class DishView extends RelativeLayout {
 
-    public DishView(Context context) {
+    private String name;
+    private String missingIngredientText;
+    private String imageURL;
+
+    private TextView lblDishName;
+    private TextView lblMissingIngredients;
+    private ImageView imgDish;
+
+
+    public DishView(Context context, Dish dish) {
         super(context);
+        //LayoutInflater.from(context).inflate(R.layout.key_value_layout, this);
     }
 
-    public DishView(Context context, AttributeSet attrs) {
+    public DishView(Context context, AttributeSet attrs, Dish dish) {
         super(context, attrs);
+
     }
 
-    public DishView(Context context, AttributeSet attrs, int defStyle) {
+    public DishView(Context context, AttributeSet attrs, int defStyle, Dish dish) {
         super(context, attrs, defStyle);
-    }
 
+    }
 
 }
